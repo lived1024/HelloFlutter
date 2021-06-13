772fb58dd689561da828b42815b4333571d25e42
lived1024 <lived1024@gmail.com>
2021-06-13 21:35:07 +0900

    Button 기본 사용법

---
+++ lib/button_demo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ButtonDemo());
+
+class ButtonDemo extends StatefulWidget{
+  @override
+  State createState() => ButtonDemoState();
+}
+
+class ButtonDemoState extends State<ButtonDemo>{
+  static const String _title = "Button 위젯 데모";
+  String _buttonState = "OFF";
+
+  void onClick(){
+    print('onClick()');
+    setState((){
+      if(_buttonState == 'OFF'){
+        _buttonState = 'ON';
+      }else{
+        _buttonState = 'OFF';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title : _title,
+      debugShowCheckedModeBanner: false,
+      home : Scaffold(
+        appBar : AppBar(title : Text(_title)),
+        body : Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+            //RaisedButton(
+              child : Text('사각 버튼'),
+              onPressed : onClick,
+            ),
+            Text('$_buttonState'),
+            RaisedButton(           // 변경된 RaisedButton은 style 문법 차이가 있다.
+              child : Text("둥근 버튼"),
+              onPressed: onClick,
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(30.0)
+              ),
+            )
+          ],
+        )
+      )
+    );
+  }
+}
