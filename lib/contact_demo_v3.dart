efa308a409a4aa29104b34cdddce553d85d3015f
lived1024 <lived1024@gmail.com>
2021-06-30 23:52:22 +0900

    동적 routes 활용 주소록 주석 추가

--- lib/contact_demo_v3.dart
+++ lib/contact_demo_v3.dart
@@ -11,7 +11,13 @@ class ContactsDemoV3 extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      /// ContactListPage : page_contact_list에 선언한 클래스
       home: ContactListPage(),
+      /// 동적으로 routes를 구성하기위해 onGenerateRoute 속성을 구현해야함
+      /// 아래 속성에서 실행되는 함수의 원형은 다음과 같다
+      /// typedef RouteFactory = Route<dynamic> Function(RouteSettings settings);
+      /// Route<dynamic>을 반환
+      /// RouteSettings에 경로이름인 name과 새로운 페이지에 넘길 인자인 arguments속성이 있음
       onGenerateRoute: (RouteSettings settings){
         if(ContactDetailPage.routeName == settings.name){
           Contact? c = settings.arguments as Contact?;
@@ -19,6 +25,7 @@ class ContactsDemoV3 extends StatelessWidget{
             builder: (context) => ContactDetailPage(c!)
           );
         }
+        /// 적절한 경로를 찾을 수 없을때 해당 함수 실행
         return _noWay;
       },
     );
