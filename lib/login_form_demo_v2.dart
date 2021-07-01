6a3e70e92e32e0b1c9596e66adedc96715494ec7
lived1024 <lived1024@gmail.com>
2021-07-02 00:31:34 +0900

    상태관리 학습을 위한 로그인폼

---
+++ lib/login_form_demo_v2.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';  // 상태관리를 위해 import가 필요하다
+
+void main() => runApp(
+  ChangeNotifierProvider(
+    builder: (context) => SimpleState(),
+    child: StateLoginDemo(),
+  )
+);
+
+const String ROOT_PAGE = '/';
+const String MAIN_PAGE = '/main';
+
+class StateLoginDemo extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '로그인 폼',
+      debugShowCheckedModeBanner: false,
+      initialRoute: ROOT_PAGE,
+      routes: {
+        ROOT_PAGE : (context) => LoginPage(),
+        MAIN_PAGE : (context) => MainPage(),
+      },
+    );
+  }
+}
