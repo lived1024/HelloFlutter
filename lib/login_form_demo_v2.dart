a105c97c855a063431f3a0955fd2f72f6e34be8a
lived1024 <lived1024@gmail.com>
2021-07-06 00:11:21 +0900

    상태관리를 활용한 로그인 화면

--- lib/login_form_demo_v2.dart
+++ lib/login_form_demo_v2.dart
@@ -5,9 +5,17 @@ import 'page_login.dart';  // 상태관리를 위해 import가 필요하다
 import 'page_main.dart';
 import 'state_simple.dart';
 
+/// provider 패키지
+/// ChangeNotifierProvider    - 단일 모델 제공
+/// MultiProvider             - 다수의 모델 클래스 지정 시 사용
+/// Provider                  - 지정한 모델에 접근하여 모델에 값을 갱신할 수 있게 해줌
+/// Consumer                  - Provider 클래스에 제공되는 모델 클래스를 읽어온다.
 void main() => runApp(
   ChangeNotifierProvider(
-    create: (context) => SimpleState(), // 참고서엔 build를 이용했지만 현재는 변경됐다.
+    // SimpleState : 상태 관리의 대상 모델
+    // 참고서엔 build를 이용했지만 현재는 변경됐다.
+    create: (context) => SimpleState(),
+    // StateLoginDemo : 실행할 앱 지정
     child: StateLoginDemo(),
   )
 );
--- lib/page_main.dart
+++ lib/page_main.dart
@@ -8,6 +8,7 @@ class MainPage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+        // Consumer로 값을 전달받기 원하는 모델 지정
         child: Consumer<SimpleState>(
           builder: (context, state, child){
             return Text('로그인 완료: ${state.email}');
