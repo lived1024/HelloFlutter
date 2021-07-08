a0c7530e03f55dc8e6ab6d7cd774c850d16017b6
lived1024 <lived1024@gmail.com>
2021-07-09 01:06:28 +0900

    차후 계획

--- lib/http/http_simple_json_demo.dart
+++ lib/http/http_simple_json_demo.dart
@@ -6,9 +6,11 @@ void main() async{
   String url = 'https://jsonplaceholder.typicode.com/todos/1';
   var response = await http.get(url);
 
+  // jsonDecode는 dart:convert패키지를 임포트 해야한다.
   Map<String, dynamic> data = jsonDecode(response.body);
   print('userId : ${data['userId']}');
   print('id : ${data['id']}');
   print('title : ${data['title']}');
   print('complete : ${data['completed']}');
 }
+/// 이후 작업은 서울 지하철 정보와 연동하여 진행예정
