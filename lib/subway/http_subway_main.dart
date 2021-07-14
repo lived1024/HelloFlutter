ab4eb420953af3febfc34a44a44fa1481ee45dc6
lived1024 <lived1024@gmail.com>
2021-07-15 00:07:06 +0900

    지하철 공공 API활용 V2

---
+++ lib/subway/http_subway_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(SubwayDemo());
+
+class SubwayDemo extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '지하철 실시간 정보',
+      home: MainPage(),
+    );
+  }
+}
