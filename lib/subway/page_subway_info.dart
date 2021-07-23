f4d41a8960940517487b5c78482c5ee43b6cfbc7
lived1024 <lived1024@gmail.com>
2021-07-24 01:11:39 +0900

    클론코딩

--- lib/subway/page_subway_info.dart
+++ lib/subway/page_subway_info.dart
@@ -1,7 +1,62 @@
 import 'package:flutter/material.dart';
+import 'model/subway_arrival.dart';
+import 'api/subway_api.dart' as api;
 
 
 class MainPage extends StatefulWidget{
   @override
   State createState() => MainPageState();
+}
+
+class MainPageState extends State<MainPage>{
+  TextEditingController _stationController = TextEditingController(text: api.defaultStation);
+  List<SubwayArrival> _data = [];
+  bool _isLoading = false;
+
+  List<Card> _buildCards(){
+    print('>>> _data.length? ${_data.length}');
+
+    if(_data.length == 0){
+      return <Card>[];
+    }
+    List<Card> res = [];
+    for(SubwayArrival info in _data){
+      Card card = Card(
+        child: Column(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18/11,
+              child: Image.asset(
+                'assets/icon/subway.png',
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(16.1, 12.0, 16.0, 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      info.trainLineNm,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 4.0),
+                    Text(
+                      info.arvlMsg2,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+      res.add(card);
+    }
+    return res;
+  }
 }
