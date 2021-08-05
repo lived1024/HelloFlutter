d9f3fd07d596c588a4d34562a8b77ed27fc97c21
lived1024 <lived1024@gmail.com>
2021-08-06 01:04:59 +0900

    클래스 사용불가에 따른 대체방안 마련중

--- android/app/src/main/java/com/example/flutter_start/MainActivity.java
+++ android/app/src/main/java/com/example/flutter_start/MainActivity.java
@@ -6,11 +6,13 @@ import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+// 현재 GeneratedPluginRegistrant의 사용 불가로 대체방법을 찾는중
 public class MainActivity extends FlutterActivity {
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         // 최신 버전은 오버라이드 시 아래의 한줄이 세팅되어있다.
         //super.configureFlutterEngine(flutterEngine);
+        
         // 교재는 아래 한 줄이 자동생성 된다고 한다. 버전 차이가 있는 것으로 추정.
         GeneratedPluginRegistrant.registerWith(flutterEngine);
     }
