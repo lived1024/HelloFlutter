a28c119989afa8a76cf721047fa6da57234a7e2d
lived1024 <lived1024@gmail.com>
2021-08-17 00:49:36 +0900

    안드로이드 쪽에서는 비정상적으로 뜨는 것이 남아있음. 확인 필요할 듯

--- lib/platform_channels/battery_channel_demo_v1.dart
+++ lib/platform_channels/battery_channel_demo_v1.dart
@@ -11,16 +11,16 @@ class BatteryPage extends StatefulWidget {
 class BatteryPageState extends State<BatteryPage> {
   String _text = '배터리 잔량: 모름';
 
-  static const String CHANNEL_BATTERY = 'android/battery';
+  static const String CHANNEL_BATTERY = 'android/battery';    // 안드로이드 폴더에서 정의한 채널고 메서드 상수 지정
   static const String METHOD_BATTERY = 'getBatteryLevel';
-  static const MethodChannel batteryChannel = MethodChannel(CHANNEL_BATTERY);
+  static const MethodChannel batteryChannel = MethodChannel(CHANNEL_BATTERY); // MethodChannel 채널 생성
 
   _refresh() async {
     print('refresh battery level');
 
     String _newText;
     try {
-      final int result = await batteryChannel.invokeMethod(METHOD_BATTERY);
+      final int result = await batteryChannel.invokeMethod(METHOD_BATTERY);   // 채널에 정의된 메서드 호출(invokeMethod)
       _newText = '배터리 잔량: $result %';
     } on PlatformException {
       _newText = '베터리 잔량을 알 수 없습니다.';
