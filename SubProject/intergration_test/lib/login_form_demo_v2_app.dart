dd6245fc28a2955a654744651f1627065cadee0a
lived1024 <lived1024@gmail.com>
2021-09-15 01:31:29 +0900

    login_form_demo_v2 드라이버 실행파일

---
+++ SubProject/intergration_test/lib/login_form_demo_v2_app.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:intergration_test/login_form_demo_v2.dart' as target;
+
+void main(){
+  enableFlutterDriverExtension();
+  target.main();
+}
